fb0924c82afe846333722d3166b6b6f8c2916656
qinx0 <qinx.developer@gmail.com>
2026-03-31 09:50:40 +0200

    Initial commit

---
+++ src/main/java/qinx/litematicacompanion/LitematicaCompanionClient.java
@@ -0,0 +1,10 @@
+package qinx.litematicacompanion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class LitematicaCompanionClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
